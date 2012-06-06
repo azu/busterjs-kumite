9d07493582bb3e1e8f68924a4d397c0c8060925d
azu <azuciao@gmail.com>
2012-06-06 23:56:55 +0900

    add simple node testing

---
+++ getting-started/buster.js
@@ -0,0 +1,8 @@
+var config = module.exports;
+
+config["My tests"] = {
+    env : "node", // or "browser"
+    tests : [
+        "test/*-test.js"
+    ]
+};
---
+++ getting-started/test/simple-node-test.js
@@ -0,0 +1,15 @@
+var buster = require("buster");// for Node env
+buster.testCase("My Test Case", {
+    "assert.euqals a === B" : function(){
+        assert.equals("foo", "foo");
+    },
+    "should be true" : function(){
+        assert(true);
+    },
+    "should be false" : function(){
+        refute(false);
+    },
+    "refute.euqals a !== B" : function(){
+        refute.equals("foo", "bar");
+    }
+});
