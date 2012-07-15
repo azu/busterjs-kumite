0599df306afbb5b7eeda79bccb27bd9a615ba686
azu <azuciao@gmail.com>
2012-07-15 18:20:02 +0900

    Date formats Interface

--- strftime/src/strftime.js
+++ strftime/src/strftime.js
@@ -1,6 +1,28 @@
 // strftimeはDataオブジェクトを拡張する
 if (typeof Date.prototype.strftime !== "function"){
     Date.prototype.strftime = (function(){
+        Date.formats = {
+            // Formatting methods
+            d : function(date){
+            },
+
+            m : function(date){
+            },
+
+            y : function(date){
+            },
+
+            Y : function(date){
+            },
+
+            j : function(date){
+            },
+
+            // Format shorthands
+            F : "%Y-%m-%d",
+            D : "%m/%d/%y"
+        };
+
     })();
 }
 
