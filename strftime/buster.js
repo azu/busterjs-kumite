f559ee73cc83058ec29eda0706c65f568c216a19
azu <azuciao@gmail.com>
2012-07-15 18:33:00 +0900

    add browser setting

--- strftime/buster.js
+++ strftime/buster.js
@@ -1,7 +1,14 @@
 var config = module.exports;
 
-config["My tests"] = {
+config["My node test"] = {
     env : "node", // or "browser"
+    tests : [
+        "test/*-test.js"
+    ]
+};
+
+config["My browser test"] = {
+    env : "browser", // or "browser"
     sources : [
         "src/*.js"
     ],
