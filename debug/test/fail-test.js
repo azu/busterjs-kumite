7d78fce1d7739256a14f92858b05ff1bc19fe234
azu <azuciao@gmail.com>
2012-11-16 02:04:52 +0900

    pass/failを逆転するテスト

--- debug/test/fail-test.js
+++ debug/test/fail-test.js
@@ -12,12 +12,26 @@ buster.testCase("My Failure Test", {
             // failが起きた時にpassしたとイベントを投げる
             buster.assertions.emit("pass", "fail", message || "");
         };
+        this.overWritePass = function pass(name, message){
+            // 明示的にfail経由で呼び出れたものは素通りして、それ以外の時は実際にfailを発生させる
+            if (name !== "fail"){
+                var fail_message = "[" + name + "] " + message;
+                this.orig_fail.call(buster.assertions, fail_message);
+
+            }
+        }.bind(this);
+        // テストがpassした時に呼ばれる
+        buster.assertions.on("pass", this.overWritePass);
     },
     tearDown : function(){
         buster.assertions.throwOnFailure = true;
         buster.assertions.fail = this.orig_fail;
+        buster.assertions.removeListener("pass", this.overWritePass);
     },
-    "this is fail,but pass test" : function(){
-        assert(false);
+    "actually, pass test" : function(){
+        assert(false);// 本来はfailだがpassする
+    },
+    "// actually, fail test" : function(){
+        assert(true);// 本来はpassだがfailする
     }
 });
