d6dc822442822bad09d98ed9db0d5dc9c495bd93
azu <azuciao@gmail.com>
2012-12-22 22:14:58 +0900

    modify export

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ buster test -c config-patterns/buster.js -e browser
 buster test -c strftime/buster.js -e node
 buster test -c strftime/buster.js -e browser
 buster test -c debug/buster.js -e node
-export NODE_PATH=$NODE_PATH:$(pwd)/reporters/reporter
+export NODE_PATH="$(pwd)/reporters/reporter:$NODE_PATH"
 buster test -c reporters/buster.js -e browser -r myReporter
 sleep 1 # FIX ME
 buster test -c test-patterns/buster.js -e browser 
