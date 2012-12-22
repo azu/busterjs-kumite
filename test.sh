be78ae8da74301aa971c109428f789f69f064ed3
azu <azuciao@gmail.com>
2012-12-23 01:07:49 +0900

    bind polyfill

--- test.sh
+++ test.sh
@@ -4,7 +4,8 @@ buster test -c config-patterns/buster.js -e node
 buster test -c config-patterns/buster.js -e browser
 buster test -c strftime/buster.js -e node
 buster test -c strftime/buster.js -e browser
-buster test buster test -c debug/buster.js -g "Error is True tests" -e browser
 NODE_PATH=$(pwd)/reporters/reporter buster test -c reporters/buster.js -e browser -r myReporter
 sleep 1 # FIX ME
 buster test -c test-patterns/buster.js -e browser 
+
+buster test buster test -c debug/buster.js -g "Error is True tests" -e browser
